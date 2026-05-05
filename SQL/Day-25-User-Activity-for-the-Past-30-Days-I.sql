04fac76e9cdf5f22c9de95cc47ed178f4acb2f3e
Sareen Habib <habibsareen@gmail.com>
2026-05-05 18:29:19 +0530

    Day 25 - User Activity for the Past 30 Days I

---
+++ SQL/Day-25-User-Activity-for-the-Past-30-Days-I.sql
@@ -0,0 +1,9 @@
+Day 25: User Activity for the Past 30 Days I
+LeetCode SQL Daily Practice
+
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
