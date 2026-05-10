103579f7bd6108b89156acbb05958e75992ed047
Sareen Habib <habibsareen@gmail.com>
2026-05-10 22:04:41 +0530

    Day 30 - Customers Who Bought All Products

---
+++ SQL/Day 30 - Customers Who Bought All Products.sql
@@ -0,0 +1,10 @@
+Day 30
+LeetCode: Customers Who Bought All Products
+
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(*)
+    FROM Product
+);
