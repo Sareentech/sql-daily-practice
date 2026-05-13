a471d8e37e366ad166c6eaf1b41a80961e8a698f
Sareen Habib <habibsareen@gmail.com>
2026-05-13 21:52:11 +0530

    Day 33 - Triangle Judgement

---
+++ SQL/Day 33 - Triangle Judgement.sql
@@ -0,0 +1,12 @@
+Day 33
+LeetCode: Triangle Judgement
+
+SELECT x, y, z,
+CASE
+    WHEN x + y > z 
+     AND x + z > y 
+     AND y + z > x 
+    THEN 'Yes'
+    ELSE 'No'
+END AS triangle
+FROM Triangle;
