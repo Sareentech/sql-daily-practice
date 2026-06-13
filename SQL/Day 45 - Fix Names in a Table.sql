d1a9ad320c71e1a67fc62ec1c03c99f74d795ee8
Sareen Habib <habibsareen@gmail.com>
2026-06-13 23:45:10 +0530

    Day 45 - Fix Names in a Table

---
+++ SQL/Day 45 - Fix Names in a Table.sql
@@ -0,0 +1,11 @@
+Day 45
+LeetCode: Fix Names in a Table
+
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
+FROM Users
+ORDER BY user_id;
